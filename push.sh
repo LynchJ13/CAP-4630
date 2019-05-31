a6977fc1a47331a7bd671b570bcbbde993c01e50
John Lynch <John.Lynch@ucf.edu>
2019-05-31 10:58:05 -0400

    remove auto-updater

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Enter a description of the changes being pushed:
-read desc
-git add .
-git commit -m "$desc"
-git push
-echo Changes successfully pushed. Press Enter to close...
-read
